bf76ef1b0af886772fc0061a2cd13d89f3add085
stylebyhedgehog <72136193+stylebyhedgehog@users.noreply.github.com>
2022-03-12 19:31:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,21 @@ import telebot
 import os
 from flask import Flask, request
 
-bot = telebot.TeleBot("5272849059:AAFcpFbq-C3sJW4lju1CX00S2c9CEbDM4vQ")
+
+bot = telebot.TeleBot("593642481:AAEuoLHI.....")
+
 server = Flask(__name__)
 
-@bot.message_handler(commands = ['start'])
-def start(message):
-  bot.send_message(message.chat.id, 'чем я могу помочь?')
 
-@bot.message_handler(content_types = ['text'])
-def texta(message):
-  if message.text == "Привет":
-    bot.send_message(message.chat.id, 'Ну привет')
-  elif message.text == "Пока":
-    bot.send_message(message.chat.id, 'BBBBBBBB')
-  elif message.text == "/test" or "/help":
-    bot.send_message(message.chat.id, 'Вы использовали команду /test')
+@bot.message_handler(commands=['start'])
+def handle_text(message):
+    user_markup = telebot.types.ReplyKeyboardMarkup(True,False)
+    user_markup.row('/start','/info')
+    start_text = str('Привет, '+message.from_user.first_name+'!\nЯ бот на Heroku.')
+    bot.send_message(chat_id=1154965888, text=start_text, parse_mode='Markdown')
 
-@server.route('/' + "5272849059:AAFcpFbq-C3sJW4lju1CX00S2c9CEbDM4vQ", methods=['POST'])
+
+@server.route('/' + tokenBot.TOKEN, methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
@@ -26,7 +24,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://test-new-new.herokuapp.com/' + "5272849059:AAFcpFbq-C3sJW4lju1CX00S2c9CEbDM4vQ")
+    bot.set_webhook(url='https://test-new-new.herokuapp.com/' + tokenBot.TOKEN)
     return "!", 200
 
 
