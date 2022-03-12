4b06bdd9e1b02763caa1ab95cb39ec7749f73150
stylebyhedgehog <72136193+stylebyhedgehog@users.noreply.github.com>
2022-03-12 19:46:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,15 +9,9 @@ bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands = ['start'])
 def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
-
-
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
-
+  bot.send_message(message.chat.id, 'чем я могу помочь?')
 
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
