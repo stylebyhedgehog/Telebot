e37a9c8350ec50e5b6594bca7bc0cfb4e1b9a643
stylebyhedgehog <72136193+stylebyhedgehog@users.noreply.github.com>
2022-03-12 19:26:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import telebot
-bot = telebot.TeleBot("5272849059:AAFcpFbq-C3sJW4lju1CX00S2c9CEbDM4vQ")
+import os
+from flask import Flask, request
 
+bot = telebot.TeleBot("5272849059:AAFcpFbq-C3sJW4lju1CX00S2c9CEbDM4vQ")
+server = Flask(__name__)
 
 @bot.message_handler(commands = ['start'])
 def start(message):
@@ -15,4 +18,18 @@ def texta(message):
   elif message.text == "/test" or "/help":
     bot.send_message(message.chat.id, 'Вы использовали команду /test')
 
-bot.polling(none_stop = True)
+@server.route('/' + "5272849059:AAFcpFbq-C3sJW4lju1CX00S2c9CEbDM4vQ", methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://test-new-new.herokuapp.com/' + "5272849059:AAFcpFbq-C3sJW4lju1CX00S2c9CEbDM4vQ")
+    return "!", 200
+
+
+if __name__ == '__main__':
+    server.debug = True
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
