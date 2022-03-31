d0a11141045f375589301657d0a30beaa286db41
stylebyhedgehog <72136193+stylebyhedgehog@users.noreply.github.com>
2022-03-31 15:52:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -44,12 +44,5 @@ def getMessage():
     return "!", 200
 
 
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://telebot2241.herokuapp.com/' + TOKEN)
-    return "!", 200
-
-
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
